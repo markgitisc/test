ccaf93392a4cacc34997e48257d6c27841dbe1bb
mark <iscp9158_work@163.com>
2017-08-28 16:06:20 +0800

    mark2

--- testGit/WebContent/mark.jsp
+++ testGit/WebContent/mark.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-mark 1
+mark 2
 </body>
 </html>
--- testGit/src/com/HelloGit.java
+++ testGit/src/com/HelloGit.java
@@ -32,7 +32,7 @@ public class HelloGit extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub2
 		doGet(request, response);
 	}
 
